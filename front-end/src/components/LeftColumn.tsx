b79fbecf27ab9c4dd18ba9d547bf9e56c00faccb
lw347347 <lw347347@byu.edu>
2021-06-23 00:47:48 -0600

    Basic front end setup, next on services

---
+++ front-end/src/components/LeftColumn.tsx
@@ -0,0 +1,7 @@
+export default function LeftColumn() {
+    return(
+        <div>
+            <h2>hello</h2>
+        </div>
+    )
+}
---
+++ front-end/src/components/RightColumn.tsx
@@ -0,0 +1,7 @@
+export default function RightColumn() {
+    return(
+        <div>
+            <h2>hello3</h2>
+        </div>
+    )
+}
